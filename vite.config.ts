dc5851e9555a0d5725447d8de85c66d36f37ce22
PassQwerty <vdwork0@gmail.com>
2025-03-01 21:05:57 +0300

    Добавил настройки для билда

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import deleteFilesPlugin from "./rollupPlugins";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "js/[name].[hash].js",
+        entryFileNames: "js/[name].[hash].js",
+      },
+      plugins: [deleteFilesPlugin([".png", ".svg"])],
+    },
+  },
+  plugins: [vue()],
   css: {
     preprocessorOptions: {
       scss: {
